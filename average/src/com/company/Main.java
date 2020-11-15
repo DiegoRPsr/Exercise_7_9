7b263d6df53db474b57d7187cd787e43bd36b68f
DiegoRPsr <73172413+DiegoRPsr@users.noreply.github.com>
2020-11-16 00:35:54 +0100

    Fixed a problem with the file version

--- average/src/com/company/Main.java
+++ average/src/com/company/Main.java
@@ -1,14 +1,18 @@
 package com.company;
 
-public class Average
+import java.util.Scanner;
+
+public class Main
 {
     public static void main(String[] args){
+        Scanner scan = new Scanner (System.in);
+
         double a=0, b, c=0, d;
         String s;
         System.out.println("Insert a mark: ");
-        s=scan.nextLine;	// we read the mark as string to check the word "end"
+        s=scan.nextLine();	// we read the mark as string to check the word "end"
         while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-            b=Convert.ToDouble(s);
+            b=Double.parseDouble(s);
             c++;   //counter of numbers
             a+=b;  // sum of all the notes
             System.out.println("Insert a mark: ");
@@ -16,7 +20,7 @@ public class Average
         }
         if(c!=0){
             d=a/c;  //calculate the average
-            System.out.println("The average is {0}.",d);
+            System.out.println("The average is " + d);
         }
     }
 }
